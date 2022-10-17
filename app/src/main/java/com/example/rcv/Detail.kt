fc1111c50508de10847213f10ec5d0d0a60b3040
Nitesh Kumar <niteshman30@gmail.com>
2022-10-17 20:00:33 +0530

    errore resolve

--- app/src/main/java/com/example/rcv/Detail.kt
+++ app/src/main/java/com/example/rcv/Detail.kt
@@ -2,6 +2,7 @@ package com.example.rcv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.rcv.databinding.ActivityDetailBinding
 
 class Detail : AppCompatActivity() {
@@ -9,7 +10,7 @@ class Detail : AppCompatActivity() {
     private var imagevalue=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+       binding=DataBindingUtil. setContentView(this,R.layout.activity_detail)
         with(binding){
             val bundle=intent.extras
             if(bundle!=null){
